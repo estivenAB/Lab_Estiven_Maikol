91bb2c92bf7eeb9d2c62d2a26639aea9ca0de6ef
Estiven <Estiven@DESKTOP-ENN0B09>
2018-11-03 16:28:52 -0600

    Agregación de clases

--- Lab3_Estiven_Maikol/src/Test.java
+++ Lab3_Estiven_Maikol/src/Test.java
@@ -1,20 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
- * @author Estiven
+ * @author Estiven Alvarez & Maikol Lizano
+ * 
+ * @version 03 noviembre 2018
  */
 public class Test {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
