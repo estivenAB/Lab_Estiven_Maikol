f8168448a3909106729c746cd6fd3b69abb3ad94
Estiven <Estiven@DESKTOP-ENN0B09>
2018-11-04 20:47:40 -0600

    Agregación de clases

--- Lab3_Estiven_Maikol/src/NationalPark.java
+++ Lab3_Estiven_Maikol/src/NationalPark.java
@@ -1,11 +1,12 @@
+
 /**
  *
  * @author Maikol Lizano
  * @author Estiven Álvarez
  * @version 3 noviembre 2018
  */
-public class NationalPark extends ProtectedAreas implements IncomeBySubsidy , IncomePerEntry{
-    
+public class NationalPark extends ProtectedAreas implements IncomeBySubsidy, IncomePerEntry {
+
     @Override
     public int income() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -13,22 +14,23 @@ public class NationalPark extends ProtectedAreas implements IncomeBySubsidy , In
 
     @Override
     public int incomeBySubsidy() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+        final int ICE_CLIENTS = 1215491;
+        int grant;
+        grant = ICE_CLIENTS * 1200;
+        return grant;
 
+    }
 
     @Override
     public int incomePerEntry(int numTicketsSoldNational, int numTicketsSoldForeign) {
 
         int incomeByNationals = numTicketsSoldNational * NATIONALS;
         int incomeByForeign = numTicketsSoldForeign * FOREIGN;
-        
+
         int incomePerEntry = incomeByForeign + incomeByNationals;
-        
+
         return incomePerEntry;
-        
+
     }
 
-    
-    
 }
