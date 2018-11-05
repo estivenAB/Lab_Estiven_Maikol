7e5890fb151f24c16d55dc47249e6b7483c9560d
Estiven <Estiven@DESKTOP-ENN0B09>
2018-11-04 21:14:37 -0600

    Agregación de clases

--- Lab3_Estiven_Maikol/src/NationalMonument.java
+++ Lab3_Estiven_Maikol/src/NationalMonument.java
@@ -5,11 +5,21 @@
  * @author Estiven Álvarez
  * @version 3 noviembre 2018
  */
-public class NationalMonument extends ProtectedAreas {
+public class NationalMonument extends ProtectedAreas implements IncomePerEntry{
 
     @Override
     public int income() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int incomePerEntry(int numTicketsSoldNational, int numTicketsSoldForeign) {
+        int incomeByNationals = numTicketsSoldNational * NATIONALS;
+        int incomeByForeign = numTicketsSoldForeign * FOREIGN;
+
+        int incomePerEntry = incomeByForeign + incomeByNationals;
+
+        return incomePerEntry;
     }
 
 }
